68f0ef273e46e1851d560f0dce571684281e446c
Max Yarosh <maxim.yarosh@bimeister.com>
2023-07-09 14:24:53 +0300

    added new tasks

---
+++ sum_of_digits.ts
@@ -0,0 +1,20 @@
+export const digitalRoot = (n: number): number => {
+  if (n <= 9) {
+    return n
+  }
+  const numSum = n.toString().split('').map(item => +item).reduce((prev, cur) => prev + cur)
+  return digitalRoot(numSum)
+};
+
+
+console.log(digitalRoot(16));
+console.log(digitalRoot(456));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
+
+
+// 16  -- > 1 + 6 = 7
+// 942  -- > 9 + 4 + 2 = 15  -- > 1 + 5 = 6
+// 132189  -- > 1 + 3 + 2 + 1 + 8 + 9 = 24  -- > 2 + 4 = 6
+// 493193  -- > 4 + 9 + 3 + 1 + 9 + 3 = 29  -- > 2 + 9 = 11  -- > 1 + 1 = 2
